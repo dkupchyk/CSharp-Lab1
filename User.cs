4ceccb45a701f666c40586fc9cce6baffd083387
dkupchyk <51787088+dkupchyk@users.noreply.github.com>
2020-02-22 17:33:32 +0200

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kupchyk01{
+    class User{
+
+        private DateTime _dateOfBirth;
+        private int _age;
+        private string _sighWest;
+        private string _sighChina;
+        private string[] _chinaSigns = { "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig" };
+
+        public DateTime DateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set { _dateOfBirth = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public string SighWest
+        {
+            get { return _sighWest; }
+            set { _sighWest = value; }
+        }
+
+        public string SighChina
+        {
+            get { return _sighChina; }
+            set { _sighChina = value; }
+        }
+
+        //public string CalcChineseHorosc()
+        //{
+        //    int index = Math.Abs(_dateOfBirth.Year - 1900) % 12;
+        //    return _chinaSigns[index];
+        //}
+
+        //public string CalcWesternHorosc()
+        //{
+        //    int month = _dateOfBirth.Month;
+        //    int day = _dateOfBirth.Day;
+
+        //    switch (month)
+        //    {
+        //        case 01 when day >= 20:
+        //        case 02 when day <= 18:
+        //            _sighWest = "Aquarius";
+        //            break;
+        //        case 02 when day >= 19:
+        //        case 03 when day <= 20:
+        //            _sighWest = "Pisces";
+        //            break;
+        //        case 03 when day >= 21:
+        //        case 04 when day <= 19:
+        //            _sighWest = "Aries";
+        //            break;
+        //        case 04 when day >= 20:
+        //        case 05 when day <= 20:
+        //            _sighWest = "Taurus";
+        //            break;
+        //        case 05 when day >= 21:
+        //        case 06 when day <= 20:
+        //            _sighWest = "Gemini";
+        //            break;
+        //        case 06 when day >= 21:
+        //        case 07 when day <= 22:
+        //            _sighWest = "Cancer";
+        //            break;
+        //        case 07 when day >= 23:
+        //        case 08 when day <= 22:
+        //            _sighWest = "Leo";
+        //            break;
+        //        case 08 when day >= 23:
+        //        case 09 when day <= 22:
+        //            _sighWest = "Virgo";
+        //            break;
+        //        case 09 when day >= 23:
+        //        case 10 when day <= 22:
+        //            _sighWest = "Libra";
+        //            break;
+        //        case 10 when day >= 23:
+        //        case 11 when day <= 21:
+        //            _sighWest = "Scorpio";
+        //            break;
+        //        case 11 when day >= 22:
+        //        case 12 when day <= 21:
+        //            _sighWest = "Sagittarius";
+        //            break;
+        //        case 12 when day >= 22:
+        //        case 01 when day <= 19:
+        //            _sighWest = "Capricorn";
+        //            break;
+        //    }
+
+        //    return _sighWest;
+        //}
+
+        public string CalcChineseHorosc()
+        {
+            string[] zodiacSigns = { "Криса", "Бик", "Тигр", "Кролик", "Дракон", "Змія", "Кінь", "Коза", "Мавпа", "Півень", "Собака", "Свиня" };
+
+            return zodiacSigns[(_dateOfBirth.Year - 4) % 12];
+        }
+        public string CalcWesternHorosc()
+        {
+            DateTime dateToCheck = _dateOfBirth;
+            string zod = "";
+            string[] zodiacSigns = { "Овен", "Телець", "Близнюки", "Paк", "Лев", "Діва", "Терези", "Скорпіон", "Стрілець", "Козоріг", "Водолій", "Риби" };
+            if ((dateToCheck.Month == 3 && dateToCheck.Day >= 21) || (dateToCheck.Month == 4 && dateToCheck.Day <= 20))
+            {
+                zod = zodiacSigns[0];
+            }
+            else if ((dateToCheck.Month == 4 && dateToCheck.Day >= 21) || (dateToCheck.Month == 5 && dateToCheck.Day <= 20))
+            {
+                zod = zodiacSigns[1];
+            }
+            else if ((dateToCheck.Month == 5 && dateToCheck.Day >= 21) || (dateToCheck.Month == 6 && dateToCheck.Day <= 21))
+            {
+                zod = zodiacSigns[2];
+            }
+            else if ((dateToCheck.Month == 6 && dateToCheck.Day >= 22) || (dateToCheck.Month == 7 && dateToCheck.Day <= 22))
+            {
+                zod = zodiacSigns[3];
+            }
+            else if ((dateToCheck.Month == 7 && dateToCheck.Day >= 23) || (dateToCheck.Month == 8 && dateToCheck.Day <= 23))
+            {
+                zod = zodiacSigns[4];
+            }
+            else if ((dateToCheck.Month == 8 && dateToCheck.Day >= 24) || (dateToCheck.Month == 9 && dateToCheck.Day <= 22))
+            {
+                zod = zodiacSigns[5];
+            }
+            else if ((dateToCheck.Month == 9 && dateToCheck.Day >= 23) || (dateToCheck.Month == 10 && dateToCheck.Day <= 23))
+            {
+                zod = zodiacSigns[6];
+            }
+            else if ((dateToCheck.Month == 10 && dateToCheck.Day >= 24) || (dateToCheck.Month == 11 && dateToCheck.Day <= 22))
+            {
+                zod = zodiacSigns[7];
+            }
+            else if ((dateToCheck.Month == 11 && dateToCheck.Day >= 23) || (dateToCheck.Month == 12 && dateToCheck.Day <= 21))
+            {
+                zod = zodiacSigns[8];
+            }
+            else if ((dateToCheck.Month == 12 && dateToCheck.Day >= 22) || (dateToCheck.Month == 1 && dateToCheck.Day <= 20))
+            {
+                zod = zodiacSigns[9];
+            }
+            else if ((dateToCheck.Month == 1 && dateToCheck.Day >= 21) || (dateToCheck.Month == 2 && dateToCheck.Day <= 20))
+            {
+                zod = zodiacSigns[10];
+            }
+            else if ((dateToCheck.Month == 2 && dateToCheck.Day >= 21) || (dateToCheck.Month == 3 && dateToCheck.Day <= 20))
+            {
+                zod = zodiacSigns[11];
+            }
+            return zod;
+        }
+
+    }
+}
---
+++ UserViewModel.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Kupchyk01
+{
+    public class UserViewModel : INotifyPropertyChanged{
+
+        private User _user = new User();
+        private RelayCommand<object> _calcAge;
+
+        public DateTime DateOfBirth
+        {
+            set {
+                _user.DateOfBirth = value;
+                OnPropertyChanged();
+            }
+        }
+        public int Age
+        {
+            get { return _user.Age; }
+            set {
+                _user.Age = value;
+                OnPropertyChanged();
+            }
+        }
+        public string SighWest
+        {
+            get { return _user.SighWest; }
+            set {
+                _user.SighWest = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SighChina
+        {
+            get { return _user.SighChina; }
+            set {
+                _user.SighChina = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private async void Calculate(){
+
+            await Task.Run(() => Thread.Sleep(1000));
+
+            if (_user.DateOfBirth.Year < DateTime.Today.Year - 135  || _user.DateOfBirth > DateTime.Today)
+            {
+                MessageBox.Show("Неправильно введена дата!\n Введіть ще раз.");
+               
+            }
+            else
+            {
+                if (_user.DateOfBirth.Day == DateTime.Today.Day && _user.DateOfBirth.Month == DateTime.Today.Month)
+                {
+                    MessageBox.Show("З ДНЕМ НАРОЖЕННЯ!\n" +
+                        "Дозволь тебе сьогодні привітати,\n" +
+                        "Бо день такий буває раз на рік,\n" +
+                        "Здоров'я, щастя щиро побажати,\n" +
+                        "На все життя, на довгий - довгий вік!\n" +
+                        "До сотні літ дожить без горя і журби,\n" +
+                        "Від гір, від сонця, від калини\n" +
+                        "Всіх благ земних – з роси й води\n" +
+                        "Бажаємо тобі всі ми!");
+                }
+                _user.Age = (DateTime.Today - _user.DateOfBirth).Days / 365;
+                _user.SighWest = _user.CalcWesternHorosc();
+                _user.SighChina = _user.CalcChineseHorosc();
+
+                OnPropertyChanged(nameof(Age));
+                OnPropertyChanged(nameof(SighWest));
+                OnPropertyChanged(nameof(SighChina));
+            }
+
+        }
+
+
+        public RelayCommand<object> CalcAge
+        {
+            get
+            {
+                return _calcAge ?? (_calcAge = new RelayCommand<object>(o =>
+                        Calculate(), o => CanExecuteCommand()));
+            }
+        }
+
+        private bool CanExecuteCommand()
+        {
+            return !string.IsNullOrWhiteSpace(_user.DateOfBirth.ToString());
+        }
+
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        //[NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null){
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
